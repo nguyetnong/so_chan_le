8d7e2db808216248f189f8dfeb7f4295749fcbf1
nguyetnong <nguyetnong71099@gmail.com>
2022-10-23 18:39:35 +0700

    so chan le

---
+++ chan_le.py
@@ -0,0 +1,8 @@
+a=int(input("nhap vao mot so:"))
+if  a %2 ==0:
+    print(a,"la so chan vi du:",(a%2))
+elif a % 2 == 1:
+    
+    print(a," la so le vi du:",(a%2))
+else:
+    print("a khong phai so tu nhien")
